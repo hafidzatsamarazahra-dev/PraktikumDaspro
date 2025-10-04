3305f5b16319ed6eaced3b8ce48da230e02ae1f8
hafidza <hafidzatsamarazahra@gmail.com>
2025-10-04 12:20:27 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS11.java
@@ -0,0 +1,18 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS11 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---cetak KRS siakad---");
+        System.out.print("apakah UKT sudah lunas? (true/false): ");
+        boolean uktLunas = scanner.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT Terferifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+    }
+}
