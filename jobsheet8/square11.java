a07d20175a189203675b77e4e4253b11d82297f6
hafidza <hafidzatsamarazahra@gmail.com>
2025-10-28 19:31:03 +0700

    percobaan 1

---
+++ jobsheet8/square11.java
@@ -0,0 +1,13 @@
+package jobsheet8;
+import java.util.Scanner;
+public class square11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukkan nilai N=");
+        int N = sc.nextInt();
+        for(int i=1;i<=N;i++){
+            System.out.print("*");
+        }
+    }
+}
