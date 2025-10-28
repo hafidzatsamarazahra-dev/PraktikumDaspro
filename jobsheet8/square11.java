1dede55bd3006836462a290fec30ae0a657098f6
hafidza <hafidzatsamarazahra@gmail.com>
2025-10-28 19:34:04 +0700

    percobaan 2

--- jobsheet8/square11.java
+++ jobsheet8/square11.java
@@ -6,8 +6,11 @@ public class square11 {
 
         System.out.print("masukkan nilai N=");
         int N = sc.nextInt();
-        for(int i=1;i<=N;i++){
+        for (int iOuter=1;iOuter<=N;iOuter++){
+             for(int i=1;i<=N;i++){
             System.out.print("*");
+        }
+        System.out.println();
         }
     }
 }
