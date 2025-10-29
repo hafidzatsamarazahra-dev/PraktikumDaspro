b2e2a829200c8e7b2f3120e3af4fb97c432eadef
hafidza <hafidzatsamarazahra@gmail.com>
2025-10-29 21:00:55 +0700

    percobaan 4

---
+++ jobsheet8/nilaiKelompok11.java
@@ -0,0 +1,28 @@
+package jobsheet8;
+import java.util.Scanner;
+public class nilaiKelompok11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int j,nilai;
+         float totalNilai,rataNilai;
+
+         int i = 1;
+         while (i <=6){
+            System.out.println("kelompok " + i);
+
+            totalNilai=0;
+            for (j=1;j<=5;j++){
+            System.out.print("nilai dari kelompok penilai " + j +": ");
+            nilai = sc.nextInt();
+            totalNilai += nilai;
+         }
+           rataNilai=totalNilai/5;
+           i++;
+           System.out.println("kelompok" + i + ": nilai rata-rata = " + rataNilai );
+         }
+         
+            }
+         }
+
+//Masukan:Pengguna diminta memasukkan nilai dari 5 penilai untuk setiap kelompok (total ada 6 kelompok).
+//Luaran:Program menampilkan rata-rata nilai dari masing-masing kelompok berdasarkan nilai yang dimasukkan
