359e0698b56d64417d1323f5b1b94d0083340cdd
hafidza <hafidzatsamarazahra@gmail.com>
2025-10-29 20:41:58 +0700

    modifikasi percobaan 3

--- jobsheet8/triangle11.java
+++ jobsheet8/triangle11.java
@@ -9,10 +9,20 @@ public class triangle11 {
         while (i <= n){
             int j = 0;
             while(j <i){
-                System.out.println('*');
+                System.out.print('*');
                 j++;
             }
+            System.out.println();
             i++;
         }
     }
 }
+
+//jawaban pertanyaan
+//2.menambahkan System.out.println(); setelah inner loop untuk pindah baris setelah satu baris segitiga selesai.
+//3.Variabel i mengatur baris (outer loop).
+//  Nilainya bertambah dari 1 sampai n, menandakan jumlah baris segitiga.
+//  Variabel j mengatur jumlah bintang per baris (inner loop).
+//  Nilainya dimulai dari 0 setiap kali baris baru dimulai.
+//  Mengapa j di-set ulang ke 0 di setiap iterasi outer loop?
+//  Karena jika tidak di-reset, maka j akan terus meningkat dari iterasi sebelumnya, menyebabkan inner loop tidak berjalan sesuai harapan (tidak lagi mulai dari 0). Akibatnya, pola segitiga tidak terbentuk dengan benar (mungkin tidak mencetak apa pun setelah baris pertama)
