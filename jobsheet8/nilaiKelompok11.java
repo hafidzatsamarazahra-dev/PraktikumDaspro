7b54809456e324d727196630829901ed52389252
hafidza <hafidzatsamarazahra@gmail.com>
2025-10-29 21:10:45 +0700

    modifikasi percobaan 4

--- jobsheet8/nilaiKelompok11.java
+++ jobsheet8/nilaiKelompok11.java
@@ -5,6 +5,8 @@ public class nilaiKelompok11 {
         Scanner sc = new Scanner(System.in);
          int j,nilai;
          float totalNilai,rataNilai;
+         float rataTertinggi = 0;
+         int kelompokTertinggi = 0;
 
          int i = 1;
          while (i <=6){
@@ -17,12 +19,21 @@ public class nilaiKelompok11 {
             totalNilai += nilai;
          }
            rataNilai=totalNilai/5;
-           i++;
            System.out.println("kelompok" + i + ": nilai rata-rata = " + rataNilai );
+
+           if (rataNilai > rataTertinggi){
+            rataTertinggi = rataNilai;
+            kelompokTertinggi = i;
+           }
+           i++;
          }
          
+         System.out.println("\nkelompok dengan rata-rata tertinggi adalah kelompok " + kelompokTertinggi + "(" + rataTertinggi +")");
             }
          }
 
 //Masukan:Pengguna diminta memasukkan nilai dari 5 penilai untuk setiap kelompok (total ada 6 kelompok).
 //Luaran:Program menampilkan rata-rata nilai dari masing-masing kelompok berdasarkan nilai yang dimasukkan
+
+//jawaban pertanyaan
+//1.totalNilai digunakan untuk menjumlah nilai dari 5 penilai pada setiap kelompok. Nilai ini diinisialisasi (totalNilai = 0) di dalam outer loop agar setiap kelompok memulai perhitungan dari nol. Jika diletakkan di luar loop, hasilnya akan salah karena nilai dari kelompok sebelumnya ikut terakumulasi.
