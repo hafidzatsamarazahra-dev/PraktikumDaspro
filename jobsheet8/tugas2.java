94b49b625250a1e40b251fc7e21f88308acf385c
hafidza <hafidzatsamarazahra@gmail.com>
2025-11-01 13:32:08 +0700

    tugas 2

---
+++ jobsheet8/tugas2.java
@@ -0,0 +1,23 @@
+package jobsheet8;
+import java.util.Scanner;
+
+public class tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print(n + " ");
+                } else {
+                    System.out.print("  "); 
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
