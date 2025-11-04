b75117b0dcd1809d9ad4b46168c59bf84564f633
hafidza <hafidzatsamarazahra@gmail.com>
2025-11-04 10:50:12 +0700

    percobaan 3

---
+++ jobsheet9/arrayRataNilai11.java
@@ -0,0 +1,20 @@
+package jobsheet9;
+import java.util.Scanner;
+public class arrayRataNilai11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] nilaiMahasiswa = new int[10];
+        double total = 0;
+        double rataRata;
+
+        for (int i = 0; i < nilaiMahasiswa.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMahasiswa[i] = sc.nextInt();
+        }
+        for (int i = 0; i < nilaiMahasiswa.length; i++) {
+            total += nilaiMahasiswa[i];
+        }
+        rataRata = total / nilaiMahasiswa.length;
+        System.out.println("Rata-rata nilai = " + rataRata);
+    }
+}
