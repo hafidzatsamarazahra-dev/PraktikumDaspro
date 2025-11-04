1709eddfe7780a6cf7f077480e8aac19fed1dabb
hafidza <hafidzatsamarazahra@gmail.com>
2025-11-04 10:44:39 +0700

    modifikasi percobaan 2

--- jobsheet9/arrayNilai11.java
+++ jobsheet9/arrayNilai11.java
@@ -5,12 +5,21 @@ public class arrayNilai11 {
         int [] nilaiAkhir = new int[10];
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("masukkan nilai akhir ke " +i+ ": ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("nilai akhir ke " +i+ " adalah: " +nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("mahasiswa ke " +i+ " Lulus!");
+            } else {
+                System.out.println("mahasiswa ke " +i+ " Tidak Lulus!");
         }
     }
-}
+} }
+
+//jawaban pertanyaan
+//1.tidak ada perubahan
+//  Karena nilaiAkhir.length otomatis menyesuaikan jumlah elemen array (10 elemen), jadi hasilnya sama seperti jika kita menulis i < 10.
+//  Bedanya, dengan nilaiAkhir.length, kode menjadi lebih fleksibel — kalau nanti ukuran array diubah, loop-nya tetap akan bekerja dengan benar tanpa perlu mengganti angka 10 secara manual.
+//2.melakukan perulangan selama i masih lebih kecil dari jumlah elemen array
