972e78102283075942af6a97d3c71de12d2f90a3
hafidza <hafidzatsamarazahra@gmail.com>
2025-11-29 20:40:27 +0700

    jawaban percobaan 1

--- jobsheet11/kafe11.java
+++ jobsheet11/kafe11.java
@@ -12,8 +12,26 @@ public class kafe11 {
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang anda inginkan!");
     }
-    public static void main(String[] args) {
-        menu();
-    }
     
 }
+
+// jawaban pertanyaan
+// 1.Fungsi tanpa parameter tidak harus bertipe void.
+// Fungsi boleh saja mengembalikan nilai meskipun tidak memiliki parameter.
+// 2.Ya, dapat.
+// Daftar menu bisa ditampilkan langsung di dalam fungsi main tanpa memanggil fungsi lain.
+// 3.Penggunaan fungsi menu() memiliki beberapa keuntungan, antara lain:
+// -Struktur program lebih rapi dan terorganisir.
+// Fungsi main tidak dipenuhi terlalu banyak baris kode.
+// -Mengurangi duplikasi kode.
+// Jika menu perlu ditampilkan lebih dari satu kali, cukup memanggil fungsi menu() tanpa menulis ulang perintah.
+// -Mudah untuk pemeliharaan (maintenance).
+// Jika ada perubahan pada daftar menu, cukup mengubah isi fungsi menu() tanpa perlu mencari dan mengubah seluruh bagian program.
+// -Memudahkan pembacaan dan pemahaman program.
+// Setiap fungsi memiliki tanggung jawab yang jelas.
+// 4.-Program dimulai dari fungsi main, karena main merupakan titik awal eksekusi program Java.
+// -Di dalam fungsi main, terdapat pemanggilan menu();.
+// -Ketika baris tersebut dijalankan, alur program berpindah ke fungsi menu().
+// -Seluruh perintah System.out.println dalam fungsi menu() dieksekusi secara berurutan sehingga daftar menu tampil di layar.
+// -Setelah seluruh isi fungsi menu() selesai dijalankan, alur program kembali ke posisi pemanggilan fungsi di dalam main.
+// -Karena tidak ada instruksi lain setelah pemanggilan menu(), program kemudian selesai.
